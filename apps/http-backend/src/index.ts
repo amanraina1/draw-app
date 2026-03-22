d36b2b2f4c9cd7fe3520fa57024823331d1cf9da
amanraina1 <amanraina9086@gmail.com>
2026-03-22 19:29:46 +0530

    http and ws config/setup

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,11 +1,26 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middlewares";
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(200).json({ success: true, message: "Success !!" });
+  return res.send({ success: true, message: "Success !!" });
+});
+
+app.post("/signin", middleware, (req, res) => {
+  const userId = 1;
+  const token = jwt.sign({ userId }, JWT_SECRET);
+  res.json({ token });
+});
+
+app.post("/signup", (req, res) => {});
+
+app.post("/room", middleware, (req, res) => {
+  res.json({ roomId: 123 });
 });
 
 app.listen(3001, () => {
-  console.log("App is listening at port: 3000");
+  console.log("App is listening at port: 3001");
 });
