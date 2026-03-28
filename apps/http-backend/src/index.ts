dd41c82af7363f8f26db5b4b629a68ac72ce488f
amanraina1 <amanraina9086@gmail.com>
2026-03-29 02:15:06 +0530

    shenanigans

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -7,8 +7,32 @@ import {
   CreateUserSchema,
   SigninSchema,
 } from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  const parsedData = CreateUserSchema.safeParse(req.body);
+
+  if (!parsedData.success) {
+    res.json({ message: "Incorrect inputs" });
+    return;
+  }
+  try {
+    await prismaClient.user.create({
+      data: {
+        email: parsedData.data.username,
+        password: parsedData.data.password,
+        name: parsedData.data.name,
+      },
+    });
+    res.json({ userId: "123" });
+  } catch (e) {
+    res.status(411).json({
+      message: "User already exists",
+    });
+  }
+});
 
 app.get("/", (req, res) => {
   return res.send({ success: true, message: "Success !!" });
@@ -24,16 +48,6 @@ app.post("/signin", middleware, (req, res) => {
   const userId = 1;
   const token = jwt.sign({ userId }, JWT_SECRET);
   res.json({ token });
-});
-
-app.post("/signup", (req, res) => {
-  const data = CreateUserSchema.safeParse(req.body);
-
-  if (!data.success) {
-    res.json({ message: "Incorrect inputs" });
-    return;
-  }
-  res.json({ userId: "123" });
 });
 
 app.post("/room", middleware, (req, res) => {
